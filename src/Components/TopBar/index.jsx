723e6d2da260eecf6d72ed1b77d7f72c97923b90
David Kostuchenko <davidkost1999@gmail.com>
2021-01-20 06:29:02 +0200

    [Update] Final Update (20.01.2021)
    Added Few media querries and decided to push the project and move to a new project.
    
    "Perfectionismis can hurt you".

--- src/Components/TopBar/index.jsx
+++ src/Components/TopBar/index.jsx
@@ -5,9 +5,20 @@ import "./styles.scss";
 import Hamburger from "hamburger-react";
 
 export default function TopBar(props) {
+  // Zoom Level Based on Screen-width
+  let width = document.documentElement.clientWidth;
+  let hamburgerSize;
+
+  if (width > 2000) {
+    hamburgerSize = 48;
+  }
   return (
     <div className="top-bar-container">
-      <Hamburger toggled={props.isOpen} toggle={props.setOpen} />
+      <Hamburger
+        toggled={props.isOpen}
+        toggle={props.setOpen}
+        size={hamburgerSize}
+      />
     </div>
   );
 }
